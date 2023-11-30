cd53af1563f928d335112c9958d6cf58ebf58d1d
hadirse <152465018+hadirse@users.noreply.github.com>
2023-11-29 16:23:41 -1200

    Create Program 5.py

---
+++ Program 5.py
@@ -0,0 +1,13 @@
+def first_last_same(numberList):
+    print("Given list:", numberList)
+    
+    first_num = numberList[0]
+    last_num = numberList[-1]
+    
+    if first_num == last_num:
+        return True
+    else:
+        return False
+
+numbers_x = [10, 20, 30, 40, 10]
+print("result is", first_last_same(numbers_x))
